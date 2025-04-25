d9d290311bdc81d566edf11b78357137398fc61b
Kranium Mendoza <github@kranium.au>
2025-04-25 18:53:13 +1000

    config closer to sphinx-quickstart

--- docs/conf.py
+++ docs/conf.py
@@ -10,26 +10,17 @@ project = 'Sample Project'
 copyright = '2025, Kranium Mendoza'
 author = 'Kranium Mendoza'
 
-correct_copyright_year = False
-
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# extensions = ['sphinx_glob']
 extensions = ['myst_parser','sphinxcontrib.mermaid' ]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-html_css_files = ["custom.css"]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-
-html_show_sphinx = False
-# html_theme_options = {
-#     'collapse_navigation': False,  # <- this is key
-# }
